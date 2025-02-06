36202506541f5a296ebe0cd39c0278b104d34afc
TriggerMoke <michalosaidan@gmail.com>
2025-02-06 15:36:28 -0700

    RateLimit request increase

--- src/private/routes/api.js
+++ src/private/routes/api.js
@@ -12,7 +12,7 @@ function getElapsedMs(startTime) {
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 500 // limit each IP to 500 requests per windowMs
 });
 
 router.use(apiLimiter);
